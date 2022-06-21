5363982ce61696d75a48e8272020aa6b5a31c87b
MJ <75807617+kang123077@users.noreply.github.com>
2022-06-21 19:14:52 +0900

    [추가] ready for Hydrate
    - app.js
    + app.tsx
    + react-query

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+//_app.tsx
+
+import React from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { NextComponentType } from "next";
+import { AppContext, AppInitialProps, AppProps } from "next/app";
+import { useState } from "react";
+
+const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
+
+export default MyApp;
