0f5de7b115017c44331288b54a9fd3ddf14e389a
MJ <75807617+kang123077@users.noreply.github.com>
2022-06-21 19:59:38 +0900

    [추가] pagenation test
    ~ pagenationtest
    + posts component
    + pagenation component

---
+++ pages/paginatetest.tsx
@@ -0,0 +1,50 @@
+import MyLayout from "../component/MyLayout";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Posts from "../component/Posts";
+import Pagination from "../component/pagination";
+
+function Paginatetest() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  /* 새로 추가한 부분 */
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = (posts) => {
+    let currentPosts = 0;
+    currentPosts = posts.slice(indexOfFirst, indexOfLast);
+    return currentPosts;
+  };
+  /*                 */
+
+  return (
+    <div className="App">
+      <MyLayout>
+        <h1>Paginationtest</h1>
+        <Posts posts={currentPosts(posts)} loading={loading}></Posts>
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={setCurrentPage}
+        ></Pagination>
+      </MyLayout>
+    </div>
+  );
+}
+
+export default Paginatetest;
