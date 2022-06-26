2a4f377497ddbee5174637118979c808c693e046
MJ <75807617+kang123077@users.noreply.github.com>
2022-06-26 18:47:25 +0900

    [추가] Detail, PrePro content comp 추가
    + api 교체
    + styled comp 수정

--- component/ForumPreProContent.tsx
+++ component/ForumPreProContent.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
 
-const ForumItemContent = (props) => {
+const ForumPreProContent = (props) => {
   return (
-    <Link href={`forum/${props.id}`}>
+    <Link href={`${props.id}`}>
       <ItemWrapper>
         <Left>
           <LeftText>닉네임</LeftText>
@@ -12,39 +13,39 @@ const ForumItemContent = (props) => {
         <Right>
           <RightText>{props.nickname}</RightText>
           <RightText>{props.title}</RightText>
-          <RightContent>{props.content}</RightContent>
         </Right>
       </ItemWrapper>
     </Link>
   );
 };
 
-export default ForumItemContent;
+export default ForumPreProContent;
 
 const ItemWrapper = styled.a`
+  height: 100px;
   display: flex;
   background-color: #f6f8ff;
   border-style: none none none solid;
   border-color: #9381dd;
   border-width: 6px;
-  cursor: pointer;
+  cursor: Pointer;
 `;
 
 const Left = styled.div`
-  width: 11vw;
+  width: 8vw;
   height: 100%;
 `;
 
 const Right = styled.div`
-  width: 79vw;
+  width: 35vw;
   height: 100%;
 `;
 
 const LeftText = styled.div`
   height: 20px;
-  margin: 20px 2vw 0px 0px;
+  margin: 20px 1.8vw 0px 0px;
   text-align: right;
-  font-size: 16px;
+  font-size: 14px;
   font-weight: bold;
   color: #9381dd;
 `;
@@ -54,7 +55,7 @@ const RightText = styled.div`
   margin: 21.5px 20px 0px 0px;
   margin-right: 20px;
   text-align: left;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: normal;
   color: #9381dd;
   overflow: hidden;
@@ -63,14 +64,15 @@ const RightText = styled.div`
 `;
 
 const RightContent = styled.div`
+  height: 48px;
   margin: 21.5px 20px 20px 0px;
   text-align: left;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: normal;
   color: #494949;
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 4;
+  -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
 `;
