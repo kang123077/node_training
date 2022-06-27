d4b861b5b50e208dad5ad1af4bb98e667a3e95f9
MJ <75807617+kang123077@users.noreply.github.com>
2022-06-27 22:50:38 +0900

    [수정] confirm,cancel button 위치 변경
    #13

--- component/ForumWriteContent.tsx
+++ component/ForumWriteContent.tsx
@@ -1,29 +1,43 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
+const ForumWriteContent = (props) => {
+  const [inputs, setInputs] = useState({
+    nickname: "",
+    title: "",
+    password: "",
+  });
 
-const ForumDetailContent = (props) => {
   return (
     <ItemWrapper>
       <Left>
         <LeftText>닉네임</LeftText>
         <LeftText>제목</LeftText>
+        <LeftText>비밀번호</LeftText>
       </Left>
       <Right>
-        <RightInput type="text" maxLength="40">
+        <RightInput type="text" maxLength="40" name="nickname">
           {props.nickname}
         </RightInput>
-        <RightInput type="text" maxLength="60">
+        <RightInput type="text" maxLength="60" name="title">
           {props.title}
         </RightInput>
-        <RightContentInput type="text">
+        <RightInput type="text" maxLength="8" name="password">
+          {props.password}
+        </RightInput>
+        <RightContentInput type="text" name="content">
           {props.content}
         </RightContentInput>
       </Right>
+      <FixedButtonWrapper>
+        <FixedButton onCilck="">확인</FixedButton>
+        <FixedButton onCilck="">취소</FixedButton>
+      </FixedButtonWrapper>
     </ItemWrapper>
   );
 };
 
-export default ForumDetailContent;
+export default ForumWriteContent;
 
 const ItemWrapper = styled.a`
   display: flex;
@@ -45,7 +59,7 @@ const Right = styled.div`
 
 const LeftText = styled.div`
   height: 20px;
-  margin: 20px 2vw 0px 0px;
+  margin: 23px 2vw 0px 0px;
   text-align: right;
   font-size: 16px;
   font-weight: bold;
@@ -56,7 +70,7 @@ const LeftText = styled.div`
 const RightInput = styled.input`
   width: 70vw;
   height: 30px;
-  margin: 12px 20px 0px 0px;
+  margin: 13px 20px 0px 0px;
   margin-right: 20px;
   padding: 0px 10px 0px 10px;
   text-align: left;
@@ -91,4 +105,31 @@ const RightContentInput = styled.textarea`
   border: 1px solid #9381dd;
   box-shadow: 2px 2px 4px rgba(116, 116, 116, 0.25);
   border-radius: 15px;
+`;
+
+const FixedButtonWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  right: 20px;
+  bottom: 20px;
+`;
+
+const FixedButton = styled.button`
+  width: 108px;
+  height: 40px;
+  font-size: 14px;
+  background: #f6f8ff;
+  border: 1px solid #9381dd;
+  box-shadow: 2px 2px 4px rgba(116, 116, 116, 0.25);
+  border-radius: 100px;
+  margin-top: 20px;
+
+  &:active,
+  &:hover,
+  &:focus {
+    cursor: pointer;
+    background: #494949;
+    color: white;
+  }
 `;
